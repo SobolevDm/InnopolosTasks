737243a2afeeb6eed56a1d038ce8eaf8f3c13072
Sobolev_Dmitriy <d.sobolev.stc@innopolis.university>
2020-06-07 04:04:57 +0300

    Домашнее задание второго урока.

---
+++ src/ru/Sobolev/lesson02/Task1.java
@@ -0,0 +1,11 @@
+package ru.Sobolev.lesson02;
+
+public class Task1 {
+    // Программа по рссчету стоимости бензина
+    public static void main(String[] args) {
+        double price = 43.21;
+        int b;
+        b = 12;
+        System.out.println("За " + b + " литров бензина Вы заплатите " + price * b + " ₽.");
+    }
+}
---
+++ src/ru/Sobolev/lesson02/Task3.java
@@ -0,0 +1,12 @@
+package ru.Sobolev.lesson02;
+
+public class Task3 {
+    // Программа по переводу секунду в часы
+    public static void main(String[] args) {
+        double a; // Секунды
+        double b; // Часы
+        a = 108000;
+        b = a / 3600;
+        System.out.print(a + " секунд составят " + b + " часов.");
+    }
+}
