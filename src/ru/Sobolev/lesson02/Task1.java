ccca00fc6abcc86a28942d767402773e628ebda4
Sobolev_Dmitriy <d.sobolev.stc@innopolis.university>
2020-06-09 15:33:22 +0300

    Скорректировал согласно замечаниям.

--- src/ru/Sobolev/lesson02/Task1.java
+++ src/ru/Sobolev/lesson02/Task1.java
@@ -4,8 +4,8 @@ public class Task1 {
     // Программа по рссчету стоимости бензина
     public static void main(String[] args) {
         double price = 43.21;
-        int b;
-        b = 12;
-        System.out.println("За " + b + " литров бензина Вы заплатите " + price * b + " ₽.");
+        int amount;
+        amount = 12;
+        System.out.println("За " + amount + " литров бензина Вы заплатите " + price * amount + " ₽.");
     }
 }
--- src/ru/Sobolev/lesson02/Task2.java
+++ src/ru/Sobolev/lesson02/Task2.java
@@ -7,6 +7,6 @@ public class Task2 {
         nalog = salary / 100 * nalog;
         hands = salary - nalog;
 
-        System.out.println("При зарплате 120000 ₽ на руки Вы получите " + hands + " ₽.");
+        System.out.println("При зарплате " + salary + " ₽ на руки Вы получите " + hands + " ₽.");
     }
 }
