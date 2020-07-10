279f936d1a060ccc57c89ef3598dacae1b227e14
Sobolev_Dmitriy <d.sobolev.stc@innopolis.university>
2020-07-10 03:21:17 +0300

    Домашнее задание №8. Часть 2.

---
+++ src/ru/Sobolev/lesson08/Task2/Obj.java
@@ -0,0 +1,14 @@
+package ru.Sobolev.lesson08.Task2;
+
+public class Obj {
+    static int count;
+
+    public static void Object() {
+        Object obj = new Object();
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+}
