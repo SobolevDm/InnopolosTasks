0a17751cdbee6892e51bd52228eafdf45e836676
Sobolev_Dmitriy <d.sobolev.stc@innopolis.university>
2020-08-25 22:54:25 +0300

    Домашнее задание №33. Версия 2.

---
+++ src/ru/Sobolev/lesson33/StringLenght.java
@@ -0,0 +1,46 @@
+package ru.Sobolev.lesson33;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StringLenght {
+
+    public static void main(String[] args) {
+
+        Map<Character, Integer> chars = new HashMap();
+        List<Character> charsMax = new ArrayList<>();
+
+        String string = "Ми-Ми-Мишки очень любят мёд!";
+        Character maximum = null;
+
+        for (char i : string.toLowerCase().toCharArray()) {
+            if (chars.get(i) == null) {
+                chars.put(i, 1);
+                if (maximum == null) {
+                    maximum = i;
+                }
+            } else {
+                chars.put(i, chars.get(i) + 1);
+                if (chars.get(i) == chars.get(maximum)) {
+                    charsMax.add(i);
+                }
+                if (chars.get(i) > chars.get(maximum)) {
+                    maximum = i;
+                    charsMax.clear();
+                    charsMax.add(i);
+                }
+            }
+        }
+        if (charsMax == null) {
+            System.out.println("Символ '" + maximum + "' встречается " + chars.get(maximum) + " раз(а).");
+        } else {
+            System.out.printf("Символы:");
+            for (Character j : charsMax) {
+                System.out.printf(" '" + j + "'");
+            }
+            System.out.println(", встречается " + chars.get(maximum) + " раз(а).");
+        }
+    }
+}
